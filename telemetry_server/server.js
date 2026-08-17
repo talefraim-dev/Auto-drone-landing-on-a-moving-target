2aa4834cc9a0b53642bd6a8c2ba5a0b16fdd9463
daniel13ros <danielros883@gmail.com>
2026-08-17 12:59:57 +0300

    Updated

--- telemetry_server/server.js
+++ telemetry_server/server.js
@@ -7,36 +7,30 @@ const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
 app.use(express.json());
-
 const connectedClients = new Set();
 
 wss.on('connection', (ws) => {
-    console.log('React UI Connected to WebSocket');
+    console.log('Client connected to WebSocket');
     connectedClients.add(ws);
 
+    ws.on('message', (message) => {
+        const dataString = message.toString();
+
+        for (const client of connectedClients) {
+            if (client !== ws && client.readyState === 1) { 
+                client.send(dataString);
+            }
+        }
+    });
+
     ws.on('close', () => {
-        console.log('React UI Disconnected');
+        console.log('Client disconnected');
         connectedClients.delete(ws);
     });
-});
-
-app.post('/telemetry', (req, res) => {
-    const telemetryData = req.body;
-
-    const dataString = JSON.stringify(telemetryData);
-
-    for (const client of connectedClients) {
-        if (client.readyState === 1) { 
-            client.send(dataString);
-        }
-    }
-
-    res.status(200).send({ status: 'Data broadcasted successfully' });
 });
 
 const PORT = 4000;
 server.listen(PORT, () => {
     console.log(`Telemetry Server is running!`);
-    console.log(`Listening for Unreal POST requests on http://127.0.0.1:${PORT}/telemetry`);
-    console.log(`Listening for React WebSocket connections on ws://127.0.0.1:${PORT}`);
+    console.log(`Listening for React and Unreal WebSocket connections on ws://127.0.0.1:${PORT}`);
 });
