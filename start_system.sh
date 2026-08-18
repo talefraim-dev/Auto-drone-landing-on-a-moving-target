5aff8c4ba8a01ccd9a65286f24bd68043adf0543
daniel13ros <danielros883@gmail.com>
2026-08-18 11:35:25 +0300

    uptodate

--- start_system.sh
+++ start_system.sh
@@ -34,9 +34,9 @@ else
 fi
 
 # ---------------------------------------------------------
-# [1/4] Pixel Streaming Signalling Server
+# [1/3] Pixel Streaming Signalling Server
 # ---------------------------------------------------------
-echo -e "${BLUE}[1/4] Starting Pixel Streaming Signalling Server...${NC}"
+echo -e "${BLUE}[1/3] Starting Pixel Streaming Signalling Server...${NC}"
 cd "C:/Program Files/Epic Games/UE_5.5/Engine/Plugins/Media/PixelStreaming/Resources/WebServers/SignallingWebServer" || exit 1
 npm run start &
 SIG_PID=$!
@@ -44,19 +44,9 @@ SIG_PID=$!
 cd "$ORIGINAL_DIR"
 
 # ---------------------------------------------------------
-# [2/4] Telemetry Server (Node.js)
+# [2/3] AirSim Python API Server
 # ---------------------------------------------------------
-echo -e "${BLUE}[2/4] Starting Telemetry Server...${NC}"
-cd telemetry_server || { echo -e "${RED}Failed to find telemetry_server directory${NC}"; exit 1; }
-node server.js &
-SERVER_PID=$!
-
-cd "$ORIGINAL_DIR"
-
-# ---------------------------------------------------------
-# [3/4] AirSim Python API Server
-# ---------------------------------------------------------
-echo -e "${BLUE}[3/4] Starting AirSim Python API Server...${NC}"
+echo -e "${BLUE}[2/3] Starting AirSim Python API Server...${NC}"
 cd telemetry_server || { echo -e "${RED}Failed to find telemetry_server directory${NC}"; exit 1; }
 python airsim_api.py &
 PYTHON_PID=$!
@@ -67,9 +57,9 @@ echo -e "${BLUE}Waiting for servers to initialize...${NC}"
 sleep 2
 
 # ---------------------------------------------------------
-# [4/4] React UI
+# [3/3] React UI
 # ---------------------------------------------------------
-echo -e "${BLUE}[4/4] Starting Drone User Interface...${NC}"
+echo -e "${BLUE}[3/3] Starting Drone User Interface...${NC}"
 cd drone_user_interface || { echo -e "${RED}Failed to find drone_user_interface directory${NC}"; exit 1; }
 npm run dev &
 UI_PID=$!
@@ -79,6 +69,6 @@ cd "$ORIGINAL_DIR"
 echo -e "\n${GREEN} All systems are online!${NC}"
 echo -e "Press ${RED}[CTRL+C]${NC} to safely stop all services.\n"
 
-trap "echo -e '\n${RED}Shutting down systems...${NC}'; kill $SIG_PID $SERVER_PID $PYTHON_PID $UI_PID; exit" SIGINT SIGTERM
+trap "echo -e '\n${RED}Shutting down systems...${NC}'; kill $SIG_PID $PYTHON_PID $UI_PID; exit" SIGINT SIGTERM
 
-wait $SIG_PID $SERVER_PID $PYTHON_PID $UI_PID
+wait $SIG_PID $PYTHON_PID $UI_PID
